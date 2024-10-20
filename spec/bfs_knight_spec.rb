9cdabef2c335b9d3aa9dad5aa436e6d1befd6314
Razvan Rotundu <raz.rotundu@gmail.com>
2024-10-20 13:25:35 -0400

    Add test skeletons

---
+++ spec/bfs_knight_spec.rb
@@ -0,0 +1,49 @@
+require_relative "../lib/bfs_knight"
+
+describe "BFS_knight functions" do
+  describe "knight_neighbors" do
+    it "Gets all neighbors from coordinate" do
+      
+      expect(true).to eq(true)
+    end
+
+    it "Ignores coords not on the board" do
+
+      expect(true).to eq(true)
+    end
+  end
+
+  describe "bfs_dict" do
+    it "handles source and target being the same" do
+      
+      expect(true).to eq(true)
+    end
+
+
+    it "returns correct record of bfs search for target" do
+
+    end
+  end
+
+  describe "bfs_path" do
+
+    it "correctly determines shortest path" do
+
+    end
+  end
+
+  describe "knight_moves" do
+    it "handles invalid source" do
+      
+      expect(true).to eq(true)
+    end
+
+    it "handles invalid target" do
+
+    end
+
+    it "Correctly prints shortest path string" do
+
+    end
+  end
+end
